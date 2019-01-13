91f62121ed02c7b113f4810e39ad9c629584224f
Adam <davidatomhernandez@gmail.com>
2019-01-13 13:58:09 -0500

    clean up dis code wow

--- bot.py
+++ bot.py
@@ -16,24 +16,6 @@ logger = logging.getLogger(__name__)
 client = discord.Client()
 text_model = {}
 
-def should_talk():
-    lucky_number = 1
-    roll = random.randint(1,20)
-    return roll == lucky_number
-
-
-def random_welcome_message():
-    messages = [
-        'Did you know that life is meaningless?',
-        'Let us rejoice.',
-        'Bend over.',
-        'That is what a call I very well put together human being',
-        'Incredible entrance although, it means nothing.',
-        'When will this end?',
-        'Praise the sun!',
-    ]
-    random_index = random.randint(0, len(messages))
-    return messages[random_index]
 
 async def create_gold_role(server):
     logger.info('Checking if gold role exists in {}'.format(server))
@@ -92,17 +74,6 @@ async def on_server_join(server):
     logger.info('Joined server: {}'. format(server))
 
 
-@client.event
-async def on_member_join(member):
-    try:
-        logger.info('A member joined')
-        server = member.server
-        fmt = "Welcome to {0.name}! {1}"
-        await client.send_message(server, fmt.format(server, random_welcome_message()))
-    except Exception as e:
-        logger.exception(e)
-
-
 def random_float(float_a, float_b):
     return random.uniform(float_a, float_b)
 
@@ -117,14 +88,8 @@ def random_date(channel):
     random_timestamp = random_float(min_date, max_date)
     return datetime.datetime.utcfromtimestamp(random_timestamp)
 
-@client.event
-async def on_message(message):
-    # TODO: replace blocks after if statements with function calls
-    if message.author == client.user:
-        # lmao don't invoke yourself m8
-        return
 
-    if message.content.startswith('!weather'):
+async def weather_response(message):
         try:
             zip_code = message.content.split()[1]
         except IndexError:
@@ -138,74 +103,100 @@ async def on_message(message):
         except Exception as e:
             logger.exception(e)
             logger.info('Could not get weather data!')
-            await client.send_message(message.channel, "Somethings not right...I need an american zip code.")
+            await client.send_message(message.channel, "Somethings not right...")
             return
-
         parsed_response = weather.parse_weather_response(response)
         await client.send_message(message.channel, parsed_response)
 
-    if message.content.startswith('!neechee'):
-        quote = nietzsche.QUOTES[random.randint(0, len(nietzsche.QUOTES) - 1)]
-        await client.send_message(message.channel, quote)
 
+async def nietzsche_response(message):
+    quote = nietzsche.QUOTES[random.randint(0, len(nietzsche.QUOTES) - 1)]
+    await client.send_message(message.channel, quote)
+
+
+async def random_markov_response(message):
+    try:
+        sentences = u''
+        random_dt = random_date(message.channel)
+        async for log in client.logs_from(message.channel, limit=2000, after=random_dt):
+            sentences += log.content + '\n'
+        text_model = markovify.Text(sentences)
+        s = text_model.make_short_sentence(280, tries=20)
+        if not s or not len(s) > 0:
+            s = "🤷"
+        await client.add_reaction(message, '🤖')
+        await client.send_message(message.channel, s.replace('@', ''))
+    except Exception as e:
+        logger.info("ERROR!: {}".format(e))
+        logger.error("Shat self: {}".format(e))           
+        await client.send_message(message.channel, "Sorry, I've just gone and shat myself.")
+
+
+async def user_markov_response(message):
+    user_id = message.content.split()[1]
+    user_id = ''.join([c for c in user_id if c.isdigit()])
+    sentences = u''
+    logger.info('looking up user {} for bottalk command'.format(user_id))
+    user = await client.get_user_info(user_id)
+    logger.info('found user {} for bottalk command'.format(user))
+    async for log in client.logs_from(message.channel, limit=4000):
+        if log.author == user:
+            sentences += log.content + '\n'
+    if len(sentences) == 0:
+        await client.send_message(message.channel, "I got nothing 🤷")
+    try:
+        text_model = markovify.Text(sentences)
+        s = text_model.make_sentence(tries=50)
+        if not s or len(s) < 1:
+            s = "My apologies, I cannot quite grasp the essence of that user."
+        await client.send_message(message.channel, s.replace('@', ''))
+    except Exception as e:
+        logger.error("Shat self: {}".format(e))
+        await client.send_message(message.channel, "Sorry, I've just gone and shat myself.")
+
+
+async def random_markov_response(message):
+    try:
+        sentences = u''
+        random_dt = random_date(message.channel)
+        async for log in client.logs_from(message.channel, limit=2000, after=random_dt):
+            sentences += log.content + '\n'
+        text_model = markovify.Text(sentences)
+        s = text_model.make_short_sentence(280, tries=20)
+        if not s or not len(s) > 0:
+            s = "🤷"
+        await client.send_message(message.channel, s.replace('@', ''))
+    except Exception as e:
+        logger.info("ERROR!: {}".format(e))
+        logger.error("Shat self: {}".format(e))
+        await client.send_message(message.channel, "Sorry, I've just gone and shat myself.")
+
+
+def should_talk():
+    lucky_number = 1
+    roll = random.randint(1,20)
+    return roll == lucky_number
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        # lmao don't invoke yourself m8
+        return
+    if message.content.startswith('!weather'):
+        await weather_response(message)
+    if message.content.startswith('!neechee'):
+        await nietzsche_response(message)
     if message.content.startswith('!topic'):
         await client.send_message(message.channel, message.channel.topic)
-
     if client.user.mentioned_in(message):
-        try:
-            sentences = u''
-            random_dt = random_date(message.channel)
-            async for log in client.logs_from(message.channel, limit=2000, after=random_dt):
-                sentences += log.content + '\n'
-            text_model = markovify.Text(sentences)
-            s = text_model.make_short_sentence(280, tries=20)
-            if not s or not len(s) > 0:
-                s = "🤷"
-            await client.add_reaction(message, '🤖')
-            await client.send_message(message.channel, s.replace('@', ''))
-        except Exception as e:
-            logger.info("ERROR!: {}".format(e))
-            logger.error("Shat self: {}".format(e))           
-            await client.send_message(message.channel, "Sorry, I've just gone and shat myself.")
-
+        await random_markov_response(message)
     if message.content.startswith('!bottalk'):
-        user_id = message.content.split()[1]
-        user_id = ''.join([c for c in user_id if c.isdigit()])
-        sentences = u''
-        logger.info('looking up user {} for bottalk command'.format(user_id))
-        user = await client.get_user_info(user_id)
-        logger.info('found user {} for bottalk command'.format(user))
-        async for log in client.logs_from(message.channel, limit=4000):
-            if log.author == user:
-                sentences += log.content + '\n'
-        if len(sentences) == 0:
-            await client.send_message(message.channel, "I got nothing 🤷")
-        try:
-            text_model = markovify.Text(sentences)
-            s = text_model.make_sentence(tries=50)
-            if not s or len(s) < 1:
-                s = "My apologies, I cannot quite grasp the essence of that user."
-            await client.send_message(message.channel, s.replace('@', ''))
-        except Exception as e:
-            logger.error("Shat self: {}".format(e))
-            await client.send_message(message.channel, "Sorry, I've just gone and shat myself.")
-
+        await user_markov_response(message)
     if should_talk():
-        try:
-            sentences = u''
-            random_dt = random_date(message.channel)
-            async for log in client.logs_from(message.channel, limit=2000, after=random_dt):
-                sentences += log.content + '\n'
-            text_model = markovify.Text(sentences)
-            s = text_model.make_short_sentence(280, tries=20)
-            if not s or not len(s) > 0:
-                s = "🤷"
-            await client.send_message(message.channel, s.replace('@', ''))
-        except Exception as e:
-            logger.info("ERROR!: {}".format(e))
-            logger.error("Shat self: {}".format(e))
-            await client.send_message(message.channel, "Sorry, I've just gone and shat myself.")
+        await random_markov_response(message)
 
-# TODO: Add message to alert a user on start/restart
+
+# TODO: Add message to alert user on start/restart
 logger.info('Starting up!')
 client.run(os.environ['DISCO_TOKEN'])
