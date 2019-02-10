7a8aa36e4d9e27e5cd8afd7c1e9b06faa0316f9c
Adam <davidatomhernandez@gmail.com>
2019-02-10 14:43:33 -0500

    he plays a dating sim

--- bot.py
+++ bot.py
@@ -63,7 +63,7 @@ async def on_ready():
             logger.info('I am in {}'.format(s))
             logger.info('attempting to update role...')
             coroutines.append(change_role_colour(s))
-        coroutines.append(client.change_presence(game=discord.Game(name='DARK SOULS™ III')))
+        coroutines.append(client.change_presence(game=discord.Game(name='Hatoful Boyfriend: A School of Hope and White Wings')))
     except Exception as e:
         logger.exception(e)
     await asyncio.gather(*coroutines)
