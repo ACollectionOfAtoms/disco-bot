2e54bd3d676ac9c1f3c155e513a0198d1d4237d6
Adam <davidatomhernandez@gmail.com>
2021-02-15 13:58:26 -0500

    clean up, add country code, wind

--- lib/weather.py
+++ lib/weather.py
@@ -2,10 +2,6 @@ import os
 import requests
 import datetime
 import discord
-import logging
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
 
 WEATHER_API_KEY = os.environ["WEATHER_API_KEY"]
 WEATHER_URI = "http://api.openweathermap.org/data/2.5/weather"
@@ -13,19 +9,39 @@ WEATHER_ENDPOINT = "{}?appid={}".format(WEATHER_URI, WEATHER_API_KEY)
 
 
 def get_weather_response(zip_code):
-    uri = "{}&q={},us".format(WEATHER_ENDPOINT, zip_code)
+    uri = "{}&q={},us&units={}".format(WEATHER_ENDPOINT, zip_code, "imperial")
     resp = requests.get(uri)
     return resp.json()
 
 
-def k_to_f(kelvin):
-    # convert kelvin to farenheit
-    return round(kelvin * 9 / 5 - 459.67)
+# stolen from: https://stackoverflow.com/a/7490772
+def degToCompass(num):
+    val = int((num / 22.5) + 0.5)
+    arr = [
+        "N",
+        "NNE",
+        "NE",
+        "ENE",
+        "E",
+        "ESE",
+        "SE",
+        "SSE",
+        "S",
+        "SSW",
+        "SW",
+        "WSW",
+        "W",
+        "WNW",
+        "NW",
+        "NNW",
+    ]
+    return arr[(val % 16)]
 
 
 def parse_weather_response(json_dict):
     logger.info("parsing weather dictionary...")
     place_name = json_dict["name"]
+    country_code = json_dict["sys"]["country"]
     place_id = json_dict["id"]
     weather_objs = json_dict["weather"]
     descriptions = [wo["description"].capitalize() for wo in weather_objs]
@@ -35,23 +51,18 @@ def parse_weather_response(json_dict):
     low_temp = json_dict["main"]["temp_min"]
     feels_like = json_dict["main"]["feels_like"]
     humidity = json_dict["main"]["humidity"]
+    wind_speed = json_dict["wind"]["speed"]
+    wind_dir = str(degToCompass(json_dict["wind"]["deg"]))
     icon_id = json_dict["weather"][0]["icon"]
     icon_url = "http://openweathermap.org/img/wn/{icon_id}@2x.png".format(
         icon_id=icon_id
     )
     time_of_calc = json_dict["dt"]
     time_stamp = datetime.datetime.fromtimestamp(time_of_calc).isoformat()
-    parsed_data = {
-        "name": place_name,
-        "description": description_string,
-        "current_temp": k_to_f(current_temp),
-        "high_temp": k_to_f(high_temp),
-        "low_temp": k_to_f(low_temp),
-    }
     embed_data = {
         "title": "Current Weather Data",
         "type": "rich",
-        "description": "{p}".format(p=place_name),
+        "description": "{p}, {c}".format(p=place_name, c=country_code),
         "url": "https://openweathermap.org/city/{place_id}".format(place_id=place_id),
         "color": 0xEB6E4B,
         "thumbnail": {
@@ -67,17 +78,22 @@ def parse_weather_response(json_dict):
             {"name": "Conditions", "value": description_string},
             {
                 "name": "Currently",
-                "value": str(k_to_f(current_temp)) + "°F",
+                "value": str(current_temp) + "°F",
                 "inline": True,
             },
-            {"name": "High", "value": str(k_to_f(high_temp)) + "°F", "inline": True},
-            {"name": "Low", "value": str(k_to_f(low_temp)) + "°F", "inline": True},
+            {"name": "High", "value": str(high_temp) + "°F", "inline": True},
+            {"name": "Low", "value": str(low_temp) + "°F", "inline": True},
             {
                 "name": "Feels Like",
-                "value": str(k_to_f(feels_like)) + "°F",
+                "value": str(feels_like) + "°F",
                 "inline": True,
             },
             {"name": "Humidity", "value": str(humidity) + "%", "inline": True},
+            {
+                "name": "Wind",
+                "value": "{s}mph {d}".format(s=wind_speed, d=wind_dir),
+                "inline": True,
+            },
         ],
         "author": {
             "name": "Open Weather API",
