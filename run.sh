dab21ac266c7b5c246a7f77f4663e1ea0d34b6e2
Adam <davidatomhernandez@gmail.com>
2021-10-28 20:48:26 -0400

    update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-docker stop $(docker ps -aq)
-docker system prune -f
+docker ps | awk '{ print $1,$2 }' | grep disco-bot | awk '{print $1 }' | xargs -I {} docker stop {}
+docker ps -a | awk '{ print $1,$2 }' | grep disco-bot | awk '{print $1 }' | xargs -I {} docker rm {}
 docker build -t disco-bot .
 docker run --restart unless-stopped --env-file env.list -d disco-bot
